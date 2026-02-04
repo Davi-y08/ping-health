9032587d5942943f250108f8e21959ae5a38cd0a
davi-y08 <oisyzenx@gmail.com>
2026-02-04 10:41:24 -0300

    get monitors by user

--- internal/repository/monitor_repository.go
+++ internal/repository/monitor_repository.go
@@ -35,6 +35,16 @@ func (r *MonitorRepository) GetMonitorById(ctx context.Context, id uuid.UUID) (*
 	return m, nil
 }
 
-/*func (r *MonitorRepository) GetMonitorsByUser(ctx context.Context, user_id uuid.UUID) ([]monitor.Monitor, error) {
+func (r *MonitorRepository) GetMonitorsByUser(ctx context.Context, user_id uuid.UUID) ([]monitor.Monitor, error) {
 	var ms []monitor.Monitor
-}*/
+
+	if err := r.db.WithContext(ctx).Model(&monitor.Monitor{}).Where("user_id = ?", user_id).Find(&ms).Error; err != nil{
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, gorm.ErrRecordNotFound
+		}
+
+		return nil, err
+	}
+
+	return ms, nil
+}
